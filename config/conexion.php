f0a67d3d32e474beb31a3269e40758f7ed16ad17
Samuel-ACH <samuelantoniocastro2021@gmail.com>
2022-07-25 20:45:41 -0600

    Conexion

---
+++ config/conexion.php
@@ -0,0 +1,18 @@
+<?php
+class conectar{
+    protected $dbh;
+
+    protected function Conexion(){
+        try{
+            $conectar = $this=>dbh = new PDO("mysql:host=20.216.41.245;dbname=g8_20","G8_20","mcdxYXrx");
+            return $conectar;
+        } catch (Exception $e){
+            print "¡Error BD!: " . $e->getMessage() . "<br/>";
+            die();
+        }
+    }
+    public function set_names(){
+        return $this->dbh->query("SET NAMES 'utf8'");
+    }
+}
+?>
