a96aa0aae2d297b1c0d2e67eb75a3581d5cc74b1
PamelaZelaya <pamela24zelaya@gmail.com>
2022-07-27 12:34:31 -0600

    Código de los archivos de Cuenta en las carpetas de controller y models
    Realización del código de Cuenta.

---
+++ controller/Cuenta.php
@@ -0,0 +1,63 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+    header('Access-Control-Allow-Headers: token, Content-Type');
+    header('Access-Control-Max-Age: 1728000');
+    header('Content-Length: 0');
+    header('Content-Type: text/plain');
+    die();
+}
+    header('Access-Control-Allow-Origin: *');  
+    header('Content-Type: application/json');
+    
+    require_once("../../config/conexion.php");
+    require_once("../models/cuenta.php");
+
+    $cuentas = new Cuentas();
+
+    $body = json_decode(file_get_contents("php://input"),true);
+    
+    switch ($_GET["opc"]) {
+
+     case "GetCuenta":
+        $datos = $cuentas->get_cuetas();
+        echo json_encode($datos);
+     break;
+
+     case "GetCuenta":
+        $datos=$cuentas->get_cuenta($body["NumeroDeCuenta"]);
+        if ($datos==null){
+            print"El número de cuenta que busca no existe.";
+        } 
+        else{
+            echo json_encode($datos);
+        }
+        break;
+        
+    case "InsertCuenta":
+            $datos=$cuentas->insert_cuenta($body["NumeroDeCuenta"],$body["NombreDeCuenta"], $body["NumeroDeCliente"], $body["FechaDeApertura"],
+            $body["SaldoActual"],$body["SaldoRetenido"], $body["TipoMoneda"]);
+            print "¡Número de cuenta agregado exitosamente!";
+            echo json_encode($datos);
+    break;
+
+    case "UpdateCuenta":
+            $datos=$cuentas->update_cuenta($body["NumeroDeCuenta"],$body["NombreDeCuenta"], $body["NumeroDeCliente"], $body["FechaDeApertura"],
+            $body["SaldoActual"],$body["SaldoRetenido"], $body["TipoMoneda"]);
+            print "¡Número de cuenta actualizado exitosamente!";
+            echo json_encode($datos);
+    break;
+
+    case "DeleteCuenta":
+            $datos = $cuentas->delete_cuenta($body["NumeroDeCuenta"]);
+            print "¡Número de cuenta eliminado exitosamente!";
+    break;
+
+
+    default:
+    print "El API que consulta no se encontró o no existe...";
+    break;
+
+    }
+?>  
---
+++ models/cuenta.php
@@ -0,0 +1,75 @@
+<?php
+ class Cuenta extends conectar {
+    
+    public  function get_cuenta() {
+        $conectar = parent::Conexion();
+        parent::set_names();
+
+        $sql = "SELECT * from cuenta;";
+        $sql = $conectar->prepare($sql);
+        $sql ->execute();
+        return $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function get_cuenta ($NumeroDeCuenta){
+        $conectar = parent::Conexion();
+        parent::set_names();
+    
+        $sql = "SELECT * from cuenta WHERE NumeroDeCuenta = ?;";
+        $sql=$conectar->prepare($sql);
+        $sql->bindValue(1, $NumeroDeCuenta);
+        $sql->execute();
+        return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+    }
+    
+
+    public function insert_cuenta($NumeroDeCuenta,$NombreDeCuenta,$NumeroDeCliente,$FechaDeApertura,$SaldoActual,$SaldoRetenido,$TipoMoneda){
+        $conectar= parent::Conexion();
+        parent::set_names();
+
+        $sql ="INSERT INTO cuenta (NumeroDeCuenta,NombreDeCuenta,NumeroDeCliente,FechaDeApertura,SaldoActual,SaldoRetenido,TipoMoneda)
+        VALUES (?, ?, ?, ?, ?, ?, ?);";
+
+        $sql=$conectar->prepare($sql);
+        $sql->bindValue(1,$NumeroDeCuenta);
+        $sql->bindValue(2,$NombreDeCuenta);
+        $sql->bindValue(3,$NumeroDeCliente);
+        $sql->bindValue(4,$FechaDeApertura);
+        $sql->bindValue(5,$SaldoActual);
+        $sql->bindValue(6,$SaldoRetenido);
+        $sql->bindValue(7,$TipoMoneda);
+        $sql->execute();
+        return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function update_cuenta($NumeroDeCuenta,$NombreDeCuenta,$NumeroDeCliente,$FechaDeApertura,$SaldoActual,$SaldoRetenido,$TipoMoneda){
+        $conectar= parent::Conexion();
+        parent::set_names();
+        
+        $sql="UPDATE cuenta SET NumeroDeCuenta=?,NombreDeCuenta=?,NumeroDeCliente=?,FechaDeApertura=?,SaldoActual=?,SaldoRetenido=?,TipoMoneda=?
+        WHERE NumeroDeCuenta=?;";
+
+        $sql=$conectar->prepare($sql);
+        $sql->bindValue(1,$NumeroDeCuenta);
+        $sql->bindValue(2,$NombreDeCuenta);
+        $sql->bindValue(3,$NumeroDeCliente);
+        $sql->bindValue(4,$FechaDeApertura);
+        $sql->bindValue(5,$SaldoActual);
+        $sql->bindValue(6,$SaldoRetenido);
+        $sql->bindValue(7,$TipoMoneda);
+        $sql->execute();
+        return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function delete_cuenta($NumeroDeCuenta){
+        $conectar= parent::Conexion();
+        parent::set_names();
+        $sql="DELETE FROM cuenta WHERE NumeroDeCuenta = $NumeroDeCuenta;";
+        $sql=$conectar->prepare($sql);
+        $sql->bindValue(1, $id);
+        $sql->execute();
+        return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+    }
+    
+ }
+?> 
