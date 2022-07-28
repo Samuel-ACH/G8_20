6ae3408ec1470d4c955496e80af85e5d5e6388a5
KarenYarelyAguilera <karenyaguilera2000@gmail.com>
2022-07-27 19:32:46 -0600

    Cambios en transaccion
    Cambios en transaccion

--- controller/Transaccion.php
+++ controller/Transaccion.php
@@ -11,7 +11,7 @@
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
 
-    require_once("../../config/conexion.php");
+    require_once("../config/conexion.php");
     require_once("../models/transaccion.php");
     $transacciones = new Transacciones();
 
